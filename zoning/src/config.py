4d9edd10de8356d5f066e2822ab5d3fa3c11cdc4
Josh Jacobson <josh.jacobson@Joshs-MacBook-Pro.local>
2023-04-01 12:23:11 -0400

    Add functionality to subtract specified FLUM areas

--- zoning/src/config.py
+++ zoning/src/config.py
@@ -2,5 +2,8 @@ class Constants:
     ANC_GEOJSON = '../data/Advisory_Neighborhood_Commissions_from_2023.geojson'
     COMP_PLAN_GEOJSON_DIRTY = '../data/Comprehensive_Plan_Planning_Areas.geojson'
     COMP_PLAN_GEOJSON_CLEAN = '../data/Comprehensive_Plan_Planning_Areas_clean.geojson'
+    FLUM_GEOJSON = '../data/Comprehensive_Plan_in_2021.geojson'
+    FLUM_EXCLUDE_TYPES = ['WATER', 'FED', 'PROS']
     OUTPUT_FILE = '../data/output.csv'
+    KEEP_GEOM_TYPE_DEFAULT = True
     PROJECTED_CRS = 'EPSG:3857'
--- zoning/src/process.py
+++ zoning/src/process.py
@@ -4,20 +4,26 @@ import argparse
 from config import Constants
 
 # Reads the ANC and Comprehensive Plan GeoJSON files
-def read_geojson_files(anc_geojson, comp_plan_geojson):
+def read_geojson_files(anc_geojson, comp_plan_geojson, flum_geojson):
     ancs = gpd.read_file(anc_geojson)
     planning_areas = gpd.read_file(comp_plan_geojson)
-    return ancs, planning_areas
+    flum = read_and_filter_flum(flum_geojson)
+    return ancs, planning_areas, flum
+
+def read_and_filter_flum(flum_geojson):
+    flum = gpd.read_file(flum_geojson)
+    return flum[flum['ALLCODES'].isin(Constants.FLUM_EXCLUDE_TYPES)]
 
 # Converts the GeoDataFrames to a projected CRS
-def project_geodataframes(ancs, planning_areas):
+def project_geodataframes(ancs, planning_areas, flum):
     ancs = ancs.to_crs(Constants.PROJECTED_CRS)
     planning_areas = planning_areas.to_crs(Constants.PROJECTED_CRS)
-    return ancs, planning_areas
+    flum = flum.to_crs(Constants.PROJECTED_CRS)
+    return ancs, planning_areas, flum
 
 # Calculates the intersections between the ANCs and Planning Areas
 def calculate_intersections(ancs, planning_areas):
-    return gpd.overlay(ancs, planning_areas, how='intersection', keep_geom_type=False)
+    return gpd.overlay(ancs, planning_areas, how='intersection', keep_geom_type=Constants.KEEP_GEOM_TYPE_DEFAULT)
 
 # Calculates the area percentages for each intersection
 def calculate_area_percentages(intersections, planning_areas):
@@ -39,13 +45,26 @@ def create_table(merged_data, output_type='percentage'):
         raise ValueError("Invalid output_type. Choose either 'percentage' or 'area'.")
     return result_table
 
+def remove_flum_area(ancs, planning_areas, flum_filtered):
+    # Perform spatial joins with ANCs and Planning Areas to find intersecting areas
+    ancs_intersections = gpd.overlay(ancs, flum_filtered, how='intersection', keep_geom_type=Constants.KEEP_GEOM_TYPE_DEFAULT)
+    planning_areas_intersections = gpd.overlay(planning_areas, flum_filtered, how='intersection', keep_geom_type=Constants.KEEP_GEOM_TYPE_DEFAULT)
+
+    # Subtract the intersecting areas from the ANCs and Planning Areas geometries
+    ancs = gpd.overlay(ancs, ancs_intersections, how='difference', keep_geom_type=Constants.KEEP_GEOM_TYPE_DEFAULT)
+    planning_areas = gpd.overlay(planning_areas, planning_areas_intersections, how='difference', keep_geom_type=Constants.KEEP_GEOM_TYPE_DEFAULT)
+    return ancs, planning_areas
+
+
 # Saves the result table as a CSV file
 def save_table_to_csv(result_table, output_file):
     result_table.to_csv(output_file)
 
-def main(output_type):
-    ancs, planning_areas = read_geojson_files(Constants.ANC_GEOJSON, Constants.COMP_PLAN_GEOJSON_CLEAN)
-    ancs, planning_areas = project_geodataframes(ancs, planning_areas)
+def main(output_type, subtract_flum_area):
+    ancs, planning_areas, flum_filtered = read_geojson_files(Constants.ANC_GEOJSON, Constants.COMP_PLAN_GEOJSON_CLEAN, Constants.FLUM_GEOJSON)
+    ancs, planning_areas, flum_filtered = project_geodataframes(ancs, planning_areas, flum_filtered)
+    if subtract_flum_area == True:
+        ancs, planning_areas = remove_flum_area(ancs, planning_areas, flum_filtered)
     intersections = calculate_intersections(ancs, planning_areas)
     merged_data = calculate_area_percentages(intersections, planning_areas)
     result_table = create_table(merged_data, output_type)
@@ -59,8 +78,18 @@ if __name__ == "__main__":
     parser.add_argument('-t', '--type', type=str, choices=['percentage', 'area'], default='percentage',
                         help='Output type: "percentage" or "area". Defaults to "percentage".')
 
+    # Add the subtract_flum_area argument
+    parser.add_argument('-sf', '--subtract_flum', action='store_true',
+                        help='Subtract FLUM areas (defined in config) from ANCs and Planning Areas.')
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Call the main function with the output_type and subtract_flum_area arguments
+    main(output_type=args.type, subtract_flum_area=args.subtract_flum)
+
     # Parse the command-line arguments
     args = parser.parse_args()
 
     # Call the main function with the output_type argument
-    main(output_type=args.type)
+    main(output_type=args.type, subtract_flum_area=args.subtract_flum)
