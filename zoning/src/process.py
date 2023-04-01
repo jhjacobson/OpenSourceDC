4e08630c445feda52843ce571559b1e8aa1dc412
Josh Jacobson <josh.jacobson@Joshs-MacBook-Pro.local>
2023-04-01 11:10:44 -0400

    Update create_percentage_table to create_table. Takes arguments for percentage or area.

--- zoning/src/process.py
+++ zoning/src/process.py
@@ -1,5 +1,6 @@
 import geopandas as gpd
 import pandas as pd
+import argparse
 from config import Constants
 
 # Reads the ANC and Comprehensive Plan GeoJSON files
@@ -27,23 +28,39 @@ def calculate_area_percentages(intersections, planning_areas):
     return merged_data
 
 # Creates a table with ANCs as rows, Planning Areas as columns, and percentages as values
-def create_percentage_table(merged_data):
-    result_table = pd.pivot_table(merged_data, values='percentage', index='ANC_ID', columns='PLAN_NAME', fill_value=0, aggfunc=sum)
-    result_table = result_table.div(result_table.sum(axis=0), axis=1) * 100
-    result_table = result_table.round(1)
+def create_table(merged_data, output_type='percentage'):
+    if output_type == 'percentage':
+        result_table = pd.pivot_table(merged_data, values='percentage', index='ANC_ID', columns='PLAN_NAME', fill_value=0, aggfunc=sum)
+        result_table = result_table.div(result_table.sum(axis=0), axis=1) * 100
+        result_table = result_table.round(1)
+    elif output_type == 'area':
+        result_table = pd.pivot_table(merged_data, values='intersection_area', index='ANC_ID', columns='PLAN_NAME', fill_value=0, aggfunc=sum)
+    else:
+        raise ValueError("Invalid output_type. Choose either 'percentage' or 'area'.")
     return result_table
 
 # Saves the result table as a CSV file
 def save_table_to_csv(result_table, output_file):
     result_table.to_csv(output_file)
 
-def main():
+def main(output_type):
     ancs, planning_areas = read_geojson_files(Constants.ANC_GEOJSON, Constants.COMP_PLAN_GEOJSON_CLEAN)
     ancs, planning_areas = project_geodataframes(ancs, planning_areas)
     intersections = calculate_intersections(ancs, planning_areas)
     merged_data = calculate_area_percentages(intersections, planning_areas)
-    result_table = create_percentage_table(merged_data)
+    result_table = create_table(merged_data, output_type)
     save_table_to_csv(result_table, Constants.OUTPUT_FILE)
 
 if __name__ == "__main__":
-    main()
+    # Create an argument parser
+    parser = argparse.ArgumentParser(description='Calculate the percentage or area of each Planning Area within each ANC.')
+
+    # Add the output_type argument
+    parser.add_argument('-t', '--type', type=str, choices=['percentage', 'area'], default='percentage',
+                        help='Output type: "percentage" or "area". Defaults to "percentage".')
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Call the main function with the output_type argument
+    main(output_type=args.type)
